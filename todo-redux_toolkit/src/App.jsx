b56246ff3ba70c63328a64f67b38fdf83db3caf5
Rakshith K <rakshithk1996.rk@gmail.com>
2024-02-25 23:09:05 +0530

    Todo list using Redux-toolkit

---
+++ todo-redux_toolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import AddTodo from './components/AddTodo.jsx';
+import Todos from './components/Todos.jsx';
+
+function App() {
+  return (
+    <div>
+      <AddTodo />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
